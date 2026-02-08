99d052d802293b0c30f98b6c463f36bc6b1dda84
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-02-08 12:43:42 -0600

    Add comments to the JWTService class

--- src/main/java/com/noobsmoke/springsecurity2026/service/JWTService.java
+++ src/main/java/com/noobsmoke/springsecurity2026/service/JWTService.java
@@ -24,27 +24,33 @@ public class JWTService {
     @Value("${security.jwt.expiration-time}")
     private long jwtExpirationTime;
 
+    //Extract the username from the JWT Token
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
+    //Extract a specific claim from the JWT Token
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
+    //Generate a JWT Token for the given user details
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
 
+    //Generate a JWT Token with additional claims for the given user details
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return buildToken(extraClaims, userDetails, jwtExpirationTime);
     }
 
+    //Gets the JWT expiration time in milliseconds
     public long getJwtExpirationTime() {
         return jwtExpirationTime;
     }
 
+    //Build a JWT Token with additional claims for the given user details
     private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, long expiration) {
         return Jwts.builder().claims(extraClaims).subject(userDetails.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
@@ -53,24 +59,29 @@ public class JWTService {
                 .compact();
     }
 
+    //Get the signing key for JWT token generation
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
+    //Check if the JWT token is valid for the given user details
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String userName = extractUsername(token);
         return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
+    //Check if the JWT token is expired
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
+    //Extract the expiration date from the JWT Token
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
+    //Extract all claims from the JWT Token
     private Claims extractAllClaims(String token) {
         return Jwts.parser().verifyWith((SecretKey) getSignInKey()).build().parseSignedClaims(token).getPayload();
     }
