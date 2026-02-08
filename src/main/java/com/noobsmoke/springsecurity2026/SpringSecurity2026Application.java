05179a5ade0656c7a1c2ec2fd632b736de189a77
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-02-08 06:52:15 -0600

    Got the repo set up

---
+++ src/main/java/com/noobsmoke/springsecurity2026/SpringSecurity2026Application.java
@@ -0,0 +1,13 @@
+package com.noobsmoke.springsecurity2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/noobsmoke/springsecurity2026/SpringSecurity2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noobsmoke.springsecurity2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
