1825a08ec27694a2c366a2731d937b8bf27b574e
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-02-08 13:30:09 -0600

    Updated code

--- src/main/java/com/noobsmoke/springsecurity2026/service/JWTService.java
+++ src/main/java/com/noobsmoke/springsecurity2026/service/JWTService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,8 @@ public class JWTService {
     @Value("${security.jwt.secret-key}")
     private String secretKey;
 
+    //Gets the JWT expiration time in milliseconds
+    @Getter
     @Value("${security.jwt.expiration-time}")
     private long jwtExpirationTime;
 
@@ -43,11 +46,6 @@ public class JWTService {
     //Generate a JWT Token with additional claims for the given user details
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return buildToken(extraClaims, userDetails, jwtExpirationTime);
-    }
-
-    //Gets the JWT expiration time in milliseconds
-    public long getJwtExpirationTime() {
-        return jwtExpirationTime;
     }
 
     //Build a JWT Token with additional claims for the given user details
